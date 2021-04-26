b7a81442d85b0eb8faae2d6fe5461952d9dc8dfe
unknown <lehaitrieu48@gmail.com>
2021-04-26 16:45:34 +0700

    helllo

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int n,k;
+bool check[80000+1];
+int main()
+{
+    freopen("MASO.INP.txt","r",stdin);
+    cin>>n>>k;
+    for(int i=1;i<=n;i++)
+    {
+        int x;
+        cin>>x;
+        check[x]=true;
+    }
+    int i=1;
+    while(k>0)
+    {
+        if(!check[i])
+        {
+            cout<<i<<endl;
+            k--;
+        }
+        i++;
+    }
+    return 0;
+}
