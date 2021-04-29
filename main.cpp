fb4b51ff3d649d9f05ae9cf2c4e809c12d8fcdbe
TrieuLee <83015709+TrieuLee@users.noreply.github.com>
2021-04-29 15:43:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,14 @@ int main()
 {
     freopen("MASO.INP.txt","r",stdin);
     cin>>n>>k;
-    for(int i=1;i<=n;i++)
-    {
+    for(int i=1;i<=n;i++){
         int x;
         cin>>x;
         check[x]=true;
     }
     int i=1;
-    while(k>0)
-    {
-        if(!check[i])
-        {
+    while(k>0){
+        if(!check[i]){
             cout<<i<<endl;
             k--;
         }
